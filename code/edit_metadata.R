c891600808056e573c244136521543429a5f0f53
sdhutchins <sdhutchins@uab.edu>
2023-08-29 00:49:53 -0500

    Add ancestry to metadata.

--- code/edit_metadata.R
+++ code/edit_metadata.R
@@ -4,19 +4,16 @@ library(tidyverse)
 # Read the CSV file
 data <- read.csv("data/MECFS_RNAseq_metadata_2023_08_16.csv")
 
-# Define a mapping between numbers and letters
-batch_mapping <- c("1" = "A", "2" = "B", "3" = "C")
-
 # Update the "Batch" column using case_when
 data <- data %>%
   mutate(Batch = case_when(
-    Batch == "B1" ~ "A",
-    Batch == "B2" ~ "B",
-    Batch == "B3" ~ "C",
+    Batch == "A" ~ "1",
+    Batch == "B" ~ "2",
+    Batch == "C" ~ "3",
     TRUE ~ Batch
   ))
 
 
 
 # Write the updated data back to the CSV file
-write.csv(data, "data/MECFS_RNAseq_metadata_2023_08_17.csv", row.names = FALSE)
+write.csv(data, "data/MECFS_RNAseq_metadata_2023_08_18.csv", row.names = FALSE)
